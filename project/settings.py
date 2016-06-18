f649d2c6b2ee0ecedd966c6db96dc237c2ceb5db
DQ <DQ@DQdeMacBook-Pro.local>
2016-06-18 13:24:23 +0800

    add STATIC_ROOT

--- project/settings.py
+++ project/settings.py
@@ -126,3 +126,4 @@ USE_TZ = False  # 默认为True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # python manage.py collectstatic 需要
